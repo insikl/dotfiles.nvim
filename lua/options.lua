5c29f4099aa806412c5695abed349a3723c6e40e
Insik Lee <lee.insik@outlook.com>
2025-03-20 16:14:10 -0400

    feat: add vim.opt.confirm
    Sync from kickstart to keep consistent and close
    
    References:
     - nvim-lua#1384

--- lua/options.lua
+++ lua/options.lua
@@ -61,6 +61,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
 -- Disable word wrap
 -- Commented out to check for wrapping of hints from LS
 vim.opt.wrap = false
