6f785d521d307e5f6da788589f407dcc68a768e1
Insik Lee <lee.insik@outlook.com>
2026-03-25 00:08:46 -0400

    chore: remove stylua from ensure_installed matching kickstart

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -216,7 +216,7 @@ return {
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        -- You can add other tools here that you want Mason to install
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
